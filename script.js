deb5264638a2bd6f17aa6f185a6c74faf7a2e48b
Tristan Pacba <tpacba@live.com>
2020-08-02 11:44:05 -0700

    applied local storage setItem and getItem, added clear button that resets local storage

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
 $( document.body ).ready(function() {
+    
     var date = new Date();
     $("#currentDay").html(date);
     var now = date.getHours();
     console.log(now);
 
-    // var now = 0;
-    // setInterval(function() {
-    //     now++;
-    //     console.log(now);
-    //     $(".description").each(function(i) {
-    //         console.log(this);
-    //         var val = $(this).attr("value");
-    //         console.log("the value is: " + parseInt(val));
-    
-    //         if (val < now) {
-    //             $(this).removeClass("present future").addClass("past");
-    //             console.log("Past");
-    //         } else if (val == now) {
-    //             $(this).removeClass("past future").addClass("present");
-    //             console.log("Present");
-    //         } else if (val > now) {
-    //             $(this).removeClass("past present").addClass("future");
-    //             console.log("Future");
-    //         }
-    //     })
-    // }, 2000);
-
     $(".description").each(function() {
-        var val = $(this).attr("value");
+        var val = $(this).attr("data-index");
 
         console.log(this);
         console.log("the value is: " + parseInt(val));
@@ -42,6 +21,32 @@ $( document.body ).ready(function() {
             $(this).removeClass("past present").addClass("future");
             console.log("Future");
         }
-
     })
+
+    var events = [];
+
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault;
+        $(".description").each(function(i, hour) {
+            events[i] = hour.value;
+        })
+        localStorage.setItem("events", JSON.stringify(events));
+    })
+
+    $("#clr").on("click", function(event) {
+        event.preventDefault;
+        $(".description").each(function(i, hour) {
+            events[i] = "";
+        })
+        localStorage.setItem("events", JSON.stringify(events));
+        window.location.reload(false); 
+    })
+
+    var newevents = JSON.parse(localStorage.getItem("events"));
+
+    if (newevents !== null) {
+        $(".description").each(function(i, hour) {
+            hour.value = newevents[i];
+        })
+    }
 });
